bdf34cff8ec6d25a990deb54d50d687e39305c70
Jacob <jerling1989@gmail.com>
2018-07-02 13:18:11 -0400

    create htaccess file and client profile page

---
+++ profile.php
@@ -0,0 +1,16 @@
+<?php
+	include('includes/header.php');
+?>
+	<div  class="text-center">
+		<p>Welcome to the Client Profile <?php echo $user['first_name'] . ' ' . $user['last_name']; ?>!</p>
+		<p>Your Client List</p>
+	</div>
+
+	
+
+
+
+</div>
+<!-- END WRAPPER DIV -->
+</body>
+</html>
