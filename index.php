ff5d4728de3455562d2b7b4a63daf7768434830e
Jacob <jerling1989@gmail.com>
2018-07-03 02:28:35 -0400

    add new client modal setup

--- index.php
+++ index.php
@@ -7,15 +7,29 @@
 ?>
 	<div  class="text-center">
 		<h2>Welcome to the Jungle <?php echo $user_obj->getFirstAndLastName(); ?>!</h2>
-		<p>Your Client List</p>
+		<br />
 	</div>
 
-	<form class="" action="index.php" method="POST">
-		<input type="text" name="first_name" placeholder="First Name" required />
-		<input type="text" name="last_name" placeholder="Last Name" required />
-		<input type="text" name="company" placeholder="Company" required />
-		<input type="submit" name="add_client" value="Add Client" />
-	</form>
+	<div id="add-client-div">
+		<form class="" action="index.php" method="POST">
+		  <div class="row">
+		    <div class="col-6 col-sm-3">
+		      <input type="text" class="form-control" name="first_name" placeholder="First Name" required />
+		    </div>
+		    <div class="col-6 col-sm-3">
+		      <input type="text" class="form-control" name="last_name" placeholder="Last Name" required />
+		    </div>
+		    <div class="col-6 col-sm-3">
+		      <input type="text" class="form-control" name="company" placeholder="Company" required />
+		    </div>
+		    <div class="col-6 col-sm-3">
+		      <input type="submit" class="btn btn-outline-light btn-block" name="add_client" value="Add Client" />
+		    </div>
+		  </div>
+		</form>
+	</div>
+
+	<!-- <br /> -->
 
 	<table class="table table-dark">
 	  <thead>
@@ -54,8 +68,61 @@
 				}
 	  	?>
 	  </tbody>
-
 	</table>
+	
+	<!-- MODAL BUTTON TRIGGER -->
+	<span data-toggle="modal" data-target="#client-modal">
+	<button type="submit" class="btn btn-success" data-toggle="tooltip" data-placement="bottom" title="Post to Profile">
+		<i class="fas fa-edit fa-2x"></i>
+	</button>
+	</span>
+
+	<!-- NEW CLIENT MODAL (.MODAL-LG) -->
+	<div class="modal fade" id="client-modal" tabindex="-1" role="dialog" aria-labelledby="postModalLabel" aria-hidden="true">
+	  <div class="modal-dialog modal-lg" role="document">
+	    <div class="modal-content">
+				<!-- MODAL HEADER -->
+	      <div class="modal-header">
+	        <h5 class="modal-title" id="exampleModalLabel">Add New Client</h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+				<!-- MODAL BODY -->
+	      <div class="modal-body">
+	        <p>Add the First Name, Last Name, and Company of a new client below!</p>
+					<!-- MODAL POST FORM -->
+	        <form class="add_new_client" action="" method="POST">
+	        	<div class="form-group">
+	        		<!-- POST BODY -->
+	        		<!-- <textarea class="form-control" name="post_body"></textarea> -->
+	        		<!-- POST INFO (USER_TO & USER_FROM) -->
+	        		<!-- <input type="hidden" name="user_from" value="<?php echo $userLoggedIn; ?>" />
+	        		<input type="hidden" name="user_to" value="<?php echo $username; ?>" /> -->
+	        	</div>
+	        	<div class="form-group">
+				      <input type="text" class="form-control" name="first_name" placeholder="First Name" required />
+				    </div>
+				    <div class="form-group">
+				      <input type="text" class="form-control" name="last_name" placeholder="Last Name" required />
+				    </div>
+				    <div class=form-group">
+				      <input type="text" class="form-control" name="company" placeholder="Company" required />
+				    </div>
+	        </form>
+	      </div>
+				<!-- MODAL FOOTER -->
+	      <div class="modal-footer">
+	      	<!-- CANCEL POST -->
+	        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+	        <!-- SEND POST -->
+	        <button type="button" class="btn btn-success" name="post_button" id="submit_profile_post">Submit</button>
+	      </div>
+	      <!-- END MODAL FOOTER -->
+	    </div>
+	  </div>
+	</div>
+	<!-- END POST MODAL -->
 
 </div>
 <!-- END WRAPPER DIV -->
